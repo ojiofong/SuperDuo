24c9d49f709ece7f927b4c958b332bd11e2b3f1a
shwn002 <shwn002@gmail.com>
2015-09-25 01:30:30 -0400

    Fixed Alexandria NullPointerException crash Improved football scores' vision issues Using material design colors

--- alexandria/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -129,6 +129,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(desc);
 
         String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
+        // Fixed NullPointerException when trying to split String
+        if (authors == null)
+            authors = "";
         String[] authorsArr = authors.split(",");
         ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
         ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",", "\n"));
